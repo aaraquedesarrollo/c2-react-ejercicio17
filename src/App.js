defdb8007714ac8e4458e82adc8521de6d51bec7
Ivan Jimenez Luque <ivanjimluqmallorca@gmail.com>
2021-06-22 12:47:24 +0200

    eliminacion de cabecera en html

--- src/App.js
+++ src/App.js
@@ -27,22 +27,6 @@ function App() {
 
   return (
     <>
-      <header className="cabecera espaciado bloque-superior">
-        <h1>Lista de la compra</h1>
-        <nav>
-          <ul className="navegacion">
-            <li>
-              <a href="principal">Principal</a>
-            </li>
-            <li className="actual">
-              <a href="lista">Lista</a>
-            </li>
-            <li>
-              <a href="acerca-de">Acerca de</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
       <Router>
         <Cabecera />
         <Switch>
