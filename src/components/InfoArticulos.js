faf8dfed3f06134aa294e5d2ccd0e08a607e3d43
Albert Araque <aaraquedesarrollo@gmail.com>
2021-06-22 13:04:01 +0200

    react icons añadidos al InfoArticulos

--- src/components/InfoArticulos.js
+++ src/components/InfoArticulos.js
@@ -1,9 +1,12 @@
+import { FaMinusCircle, FaPlusCircle } from "react-icons/fa";
+
 export const InfoArticulos = (props) => {
   const { listaArticulos } = props;
 
   return (
     <section className="info espaciado bloque-superior">
-      <i className="icono fas fa-plus-circle"></i>
+      <FaPlusCircle fontSize="2em" />
+      <FaMinusCircle fontSize="2em" />
       <p className="n-articulos">
         {listaArticulos.filter((articulo) => articulo.comprado).length}/
         {listaArticulos.length} comprados
